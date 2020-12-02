f6ebd99213391306659072b17d1a4ddfeb3b7ff6
Dzenetan <dzenetan.aliti@student.unamur.be>
2020-12-02 16:08:15 +0100

    fct sort_music

--- main.py
+++ main.py
@@ -118,7 +118,7 @@ def concatenate(word_list):
         string += word
     return string
 
-def sort_music(dir_path):
+def sort_music(dir_extract_path):
     """Extract music files from a directory and sort them into authors directory and album sub-directory
     
     Parameters
@@ -127,14 +127,80 @@ def sort_music(dir_path):
     
     Returns
     -------
-    music_dict: dicionnary containing all musics and their infos (dict)
+    list_of_music_dict: dictionnary containing all musics and their infos (dict)
+
+    Notes
+    -----
+    create a file with informations in format title;artist;albumartist;year;album;track_number;genre;\n in  .txt file for each song
     
     Version
     -------
     specification: Aliti Dzenetan (v0.1)
-    implementation: Aliti Dzenatan
+    implementation: Aliti Dzenetan
     """
-    raise NotImplementedError()
+    
+    list_of_music_dict = []
+    dir_list = os.listdir(dir_extract_path)
+    for direc in dir_list: 
+       
+        dir_path = dir_extract_path +'\\'+direc 
+
+        if os.path.isdir(dir_path): 
+            song_list = os.listdir(dir_path)
+            
+            for song in song_list:
+                
+                song_path = dir_path+'\\'+song
+                song_info = EasyMP3(song_path)
+                
+                #checking if the 'genre' exists
+                if 'genre' in song_info:
+                    genre = song_info['genre'][0]
+                else:
+                    genre = ' '
+
+                #removing '/' and ':' from the title, album and name (to allow us to create valid paths)
+                #we didn't use re.split (regex) to split on multiple delimiters because we haven't seen it in course yet
+                title = concatenate(song_info['title'][0].split('/'))           
+                title = concatenate(title.split(':'))                               
+                album = concatenate(song_info['album'][0].split('/'))
+                album = concatenate(album.split(':'))
+                artist = concatenate(song_info['artist'][0].split('/'))
+                artist = concatenate(artist.split(':'))
+
+                dicti = {
+                    'title': title,
+                    'artist': artist,
+                    'albumartist': song_info['albumartist'][0],
+                    'year': song_info['date'][0].split('-')[0],
+                    'album': album,
+                    'track_number': song_info['tracknumber'][0],
+                    'genre': genre
+                    }                
+                list_of_music_dict.append(dicti)
+
+                path = '.\\'+dicti['artist']
+
+                #creating directories artist/album for files if it doesn't exists 
+                if not os.path.exists(path):
+                    os.mkdir(path)
+                path += '\\'+dicti['album']               
+                if not os.path.exists(path):
+                    os.mkdir(path)
+                
+                new_path = path+'\\'+dicti['track_number']+'. '+ dicti['title']+' '+'('+ dicti['year']+').mp3'
+                
+                
+                os.rename(song_path, new_path)
+    
+    fh = open('.\\dicti.txt', 'w')
+    for dic in list_of_music_dict:
+        for key in dic:
+            fh.write(dic[key] + ';')
+        fh.write('\n')
+    fh.close()  
+
+    return list_of_music_dict
 
 def show_all_music(music_list):
     """Display all the music files contained in a directory and its subdirectories.
